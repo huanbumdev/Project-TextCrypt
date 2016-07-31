4081a01756d8c80c205adfbcee4c5c873d4c79b9
Huan Nguyen Pham <huanbumdev@gmail.com>
2016-07-31 11:01:46 +0700

    Update and rename TextEditor.cpp to Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,27 @@
+/*
+ * TextCrypt Copyright (C) 2016 Huan Pham
+ * All rights reserved.
+ *
+ * The algorithm to encrypt texts provided by this library
+ * should be altered before it is used personally due to
+ * security purposes.
+ *
+ * This file is part of TextCrypt.
+ *
+ * TextCrypt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TextCrypt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TextCrypt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <fstream>
 #include <cstdio>
